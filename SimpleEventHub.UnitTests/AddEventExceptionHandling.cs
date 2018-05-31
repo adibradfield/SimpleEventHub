a64becd1c550f306e2269b7b0359b3eb5f38495c
Adrian Bradfield <adrian.bradfield@outlook.com>
2018-05-31 21:45:01 +0100

    Added test for exception handling

---
+++ SimpleEventHub.UnitTests/AddEventExceptionHandling.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using SimpleEventHub.UnitTests.ModelEvents;
+using SimpleEventHub.UnitTests.TestHandlers;
+
+namespace SimpleEventHub.UnitTests
+{
+    [TestFixture]
+    public class AddEventExceptionHandling
+    {
+        [Test]
+        [TestCase(0, ExpectedResult = null)]
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 2)]
+        public int? ExceptionIsAggregated(int instances)
+        {
+            var hub = new EventHub();
+
+            for (var i = 0; i < instances; i++)
+            {
+                var handler = new SyncThrowsExceptionHandler();
+                hub.Register(handler);
+            }
+
+            try
+            {
+                hub.AddEvent(new BaseEvent());
+            }
+            catch (AggregateException e)
+            {
+                return e.InnerExceptions.Count;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SimpleEventHub.UnitTests/TestHandlers/SyncThrowsExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SimpleEventHub.UnitTests.ModelEvents;
+
+namespace SimpleEventHub.UnitTests.TestHandlers
+{
+    class SyncThrowsExceptionHandler : IHandle<BaseEvent>
+    {
+        public void Handle(BaseEvent eventInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
